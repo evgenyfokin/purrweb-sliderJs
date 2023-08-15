cac6677a9e61e71740da3827a215db2c22530fe6
evgenyfokin <fokin_e@inbox.ru>
2023-08-15 23:51:02 +0300

    fix

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const dots = document.querySelectorAll(".pagination__dot")
 let currentIndex = 0
 let slideWidth = slider.clientWidth
 
-
 function switchSlide(direction, paginationIdx) {
     if (isAnimating) return;
     isAnimating = true;
@@ -58,112 +57,12 @@ function switchSlide(direction, paginationIdx) {
         }
     });
 }
-
-
-
-// function nextSlide(paginationIdx) {
-//     if (isAnimating) return;
-//     isAnimating = true;
-//     let currentSlide = slides[currentIndex];
-//     let newIndex
-//     if (paginationIdx === undefined) {
-//         newIndex = currentIndex + 1;
-//         if (newIndex >= totalSlides) {
-//             newIndex = 0;
-//         }
-//         if (newIndex < 0) {
-//             newIndex = totalSlides - 1;
-//         }
-//     } else {
-//         newIndex = paginationIdx
-//     }
-//     let nextSlide = slides[newIndex];
-//
-//     nextSlide.style.left = slideWidth + 'px';
-//     nextSlide.style.display = 'block';
-//
-//     let position = 0;
-//     let nextPosition = slideWidth;
-//
-//
-//     const interval = setInterval(function () {
-//         position -= 5;
-//         nextPosition -= 5;
-//
-//         currentSlide.style.left = position + "px";
-//         nextSlide.style.left = nextPosition + "px";
-//
-//         if (nextPosition <= 0) {
-//             clearInterval(interval);
-//             isAnimating = false;
-//         }
-//     }, 16.67);
-//
-//     currentIndex = newIndex;
-//     dots.forEach((dot, idx) => {
-//         if (idx === currentIndex) {
-//             dot.classList.add('pagination__dot_active')
-//         } else {
-//             dot.classList.remove('pagination__dot_active')
-//         }
-//     })
-//
-// }
-//
-// function prevSlide(paginationIdx) {
-//     if (isAnimating) return;
-//     isAnimating = true;
-//     let currentSlide = slides[currentIndex];
-//     let newIndex
-//     console.log(paginationIdx)
-//     if (paginationIdx === undefined) {
-//         newIndex = currentIndex - 1;
-//         if (newIndex < 0) {
-//             newIndex = totalSlides - 1;
-//         }
-//
-//     } else {
-//         newIndex = paginationIdx
-//     }
-//
-//     let prevSlide = slides[newIndex];
-//
-//     prevSlide.style.left = -slideWidth + 'px';
-//     prevSlide.style.display = 'block';
-//
-//     let position = 0;
-//     let prevPosition = -slideWidth;
-//
-//     const interval = setInterval(function () {
-//         position += 5;
-//         prevPosition += 5;
-//
-//         currentSlide.style.left = position + "px";
-//         prevSlide.style.left = prevPosition + "px";
-//
-//         if (prevPosition >= 0) {
-//             clearInterval(interval);
-//             currentSlide.style.display = 'none';
-//             isAnimating = false;
-//         }
-//     }, 16);
-//
-//     currentIndex = newIndex;
-//     dots.forEach((dot, idx) => {
-//         if (idx === currentIndex) {
-//             dot.classList.add('pagination__dot_active')
-//         } else {
-//             dot.classList.remove('pagination__dot_active')
-//         }
-//     })
-// }
-
 dots.forEach((dot, i) => {
     dot.addEventListener('click', function () {
         if (i > currentIndex) {
-            nextSlide(i);
+            switchSlide('right', i);
         } else if (i < currentIndex) {
-            prevSlide(i);
+            switchSlide('left', i);
         }
     });
 })
